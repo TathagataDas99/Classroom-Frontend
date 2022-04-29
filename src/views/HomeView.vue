ec041b9121eca761a39172f72cec980c108059e9
Tathagata Das <dastathagata9919@gmail.com>
2022-04-29 22:15:35 +0530

    unstable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="h-3/4 text-lg">
-    <h1>Hello Home.</h1>
+    <h1 v-if="!user">Unauthorize</h1>
+    <h1 v-else>{{user}}</h1>
   </div>
 </template>
 
@@ -8,9 +9,15 @@
 import axios from "axios";
 export default {
   name: "HomeView",
-  async beforeMount() {
+  data() {
+    return {
+      user: null,
+    };
+  },
+  async created() {
     const response = await axios.get("/auth/users/me/");
     console.log(response);
+    this.user = response.data.first_name + " " + response.data.last_name;
   },
 };
 </script>
