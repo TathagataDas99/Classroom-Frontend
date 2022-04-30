c008372d51ba082da11828c76e3bf2915b1e68eb
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-30 11:48:17 +0530

    home page responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="grid h-screen grid-cols-4 grid-rows-2 items-stretch justify-evenly gap-4"
+    class="grid h-screen grid-rows-2 md:grid-cols-4 md:items-stretch md:justify-evenly md:gap-4"
   >
-    <header class="col-span-2 justify-center self-center text-center">
-      <h1 class="text-4xl font-bold">Welcome to</h1>
-      <h1 class="text-5xl font-bold">
+    <header class="justify-center self-center text-center md:col-span-2">
+      <h1 class="text-xl font-bold sm:text-4xl">Welcome to</h1>
+      <h1 class="text-2xl font-bold sm:text-5xl">
         <span class="text-primary-dark">Classroom</span>
         <span class="text-danger-dark">LMS</span>
       </h1>
@@ -12,12 +12,12 @@
     <img
       src="../assets/background/WhiteBoard.svg"
       alt="No white board img found"
-      class="col-span-2 row-span-2 h-full w-full"
+      class="hidden md:col-span-2 md:row-span-2 md:block md:h-full md:w-full"
     />
     <img
       src="../assets/background/MOBILE_LMS.svg"
       alt="No white board img found"
-      class="col-span-2 h-full w-full"
+      class="h-full w-full md:col-span-2"
     />
   </div>
 </template>
