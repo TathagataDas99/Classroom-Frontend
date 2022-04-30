04bb0000961ae9f8ffd61a261a46be5a797cc7a4
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-30 11:59:53 +0530

    md point corrected

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="grid h-screen grid-rows-2 md:grid-cols-4 md:items-stretch md:justify-evenly md:gap-4"
+    class="grid h-screen grid-rows-2 lg:grid-cols-4 lg:items-stretch lg:justify-evenly lg:gap-4"
   >
-    <header class="justify-center self-center text-center md:col-span-2">
+    <header class="justify-center self-center text-center lg:col-span-2">
       <h1 class="text-xl font-bold sm:text-4xl">Welcome to</h1>
       <h1 class="text-2xl font-bold sm:text-5xl">
         <span class="text-primary-dark">Classroom</span>
@@ -12,12 +12,12 @@
     <img
       src="../assets/background/WhiteBoard.svg"
       alt="No white board img found"
-      class="hidden md:col-span-2 md:row-span-2 md:block md:h-full md:w-full"
+      class="hidden lg:col-span-2 lg:row-span-2 lg:block lg:h-full lg:w-full"
     />
     <img
       src="../assets/background/MOBILE_LMS.svg"
       alt="No white board img found"
-      class="h-full w-full md:col-span-2"
+      class="h-full w-full lg:col-span-2"
     />
   </div>
 </template>
