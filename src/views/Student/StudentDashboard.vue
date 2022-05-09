9dddd34ac59bb7bf91875407497d20f5e1296d35
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-09 13:38:56 +0530

    sem card bg image added

--- src/views/Student/StudentDashboard.vue
+++ src/views/Student/StudentDashboard.vue
@@ -14,13 +14,18 @@
     <!-- <LoaderView /> -->
     <main
       v-else
-      class="col-span-2 mt-4 grid justify-items-center gap-y-4 lg:grid-cols-2"
+      class="relative col-span-2 mt-4 grid justify-items-center gap-y-4 lg:grid-cols-2"
     >
+      <img
+        src="../../assets/background/sem_page_card_bg_3.svg"
+        alt="No Sem BG found"
+        class="absolute bottom-0 left-24 w-full"
+      />
       <div class="card" v-for="semCard in semCards" :key="semCard.id">
         <button title="This is the on going semester">
           <BadgeCheckIcon class="sem-badge" v-show="semCard.is_current_sem" />
         </button>
-        <h1 class="card-title text-2xl text-primary-light">
+        <h1 class="card-title text-2xl text-zinc-700">
           Semester-{{ semCard.sem_no }}
         </h1>
         <button class="btn" @click="handelOpen(semCard.sem_no, semCard.id)">
