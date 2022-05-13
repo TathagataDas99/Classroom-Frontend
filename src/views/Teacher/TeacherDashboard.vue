68d20cf48a03ccc96e05cada008aab63efb0cbb8
Tathagata Das <dastathagata9919@gmail.com>
2022-05-13 20:32:33 +0530

    lint done

--- src/views/Teacher/TeacherDashboard.vue
+++ src/views/Teacher/TeacherDashboard.vue
@@ -1,15 +1,11 @@
 <template>
-    <div>
-        <h1>Hello teacher</h1>
-    </div>
+  <div>
+    <h1>Hello teacher</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
