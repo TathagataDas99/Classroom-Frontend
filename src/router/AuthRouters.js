2f6eaad250ea169054ab2723c2fddf403c935517
Tathagata Das <dastathagata9919@gmail.com>
2022-05-13 20:07:18 +0530

    router refactor done

---
+++ src/router/AuthRouters.js
@@ -0,0 +1,57 @@
+import SignIn from "../views/Auth/SignIn.vue";
+import ActivationAccount from "../views/Auth/ActivationAccount.vue";
+import LoginForm from "../views/Auth/LoginForm.vue";
+import ForgotPassword from "../views/Auth/ForgotPassword.vue";
+import ResetPassword from "../views/Auth/ResetPassword.vue";
+import HomeView from "../views/HomeView.vue";
+import store from "../store/";
+
+const unAuth = (to, from, next) => {
+  console.log("isAuth : " + store.state.isAuth); //TODO: have to remove this console log
+  if (store.state.isAuth === null || store.state.isAuth === false) {
+    next();
+  } else {
+    next({ name: "StudentDashboard" });
+  }
+};
+
+export default [
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView,
+    beforeEnter: unAuth,
+  },
+  {
+    path: "/sign-in",
+    name: "Signin",
+    component: SignIn,
+    beforeEnter: unAuth,
+  },
+  {
+    path: "/activate/:uid/:token",
+    name: "ActivationAccount",
+    component: ActivationAccount,
+    props: true,
+    beforeEnter: unAuth,
+  },
+  {
+    path: "/log-in",
+    name: "Login",
+    component: LoginForm,
+    beforeEnter: unAuth,
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    beforeEnter: unAuth,
+  },
+  {
+    path: "/reset_password_confirm/:uid/:token",
+    name: "ResetPassword",
+    component: ResetPassword,
+    props: true,
+    beforeEnter: unAuth,
+  },
+];
---
+++ src/router/StudentRouters.js
@@ -0,0 +1,62 @@
+import store from "../store";
+import StudentDashboard from "../views/Student/StudentDashboard.vue";
+import SubjectCards from "../views/Student/SubjectCards.vue";
+import ParticularSubject from "../views/Student/ParticularSubjects/ParticularSubject.vue";
+import AnnouncementView from "../views/Student/ParticularSubjects/AnnouncementView.vue";
+import AssignmentsView from "../views/Student/ParticularSubjects/AssignmentsView.vue";
+import NotesView from "../views/Student/ParticularSubjects/NotesView.vue";
+
+const isAuth = (to, from, next) => {
+  console.log("isAuth : " + store.state.isAuth); //TODO: have to remove this console log
+  if (!store.state.isAuth) {
+    next("/log-in");
+  } else {
+    next();
+  }
+};
+
+export default [
+  {
+    path: "/student-dashboard",
+    name: "StudentDashboard",
+    component: StudentDashboard,
+    beforeEnter: isAuth,
+  },
+  {
+    path: "/student-dashboard/semester-:no/subjects",
+    name: "SubjectCards",
+    component: SubjectCards,
+    props: true,
+    beforeEnter: isAuth,
+  },
+  {
+    path: "/student-dashboard/semester-:no/subjects/:subject_slug/",
+    name: "ParticularSubject",
+    component: ParticularSubject,
+    props: true,
+    beforeEnter: isAuth,
+    children: [
+      {
+        path: "announcements",
+        name: "AnnouncementView",
+        component: AnnouncementView,
+        props: true,
+        beforeEnter: isAuth,
+      },
+      {
+        path: "notes",
+        name: "NotesView",
+        component: NotesView,
+        props: true,
+        beforeEnter: isAuth,
+      },
+      {
+        path: "assignments",
+        name: "AssignmentsView",
+        component: AssignmentsView,
+        props: true,
+        beforeEnter: isAuth,
+      },
+    ],
+  },
+];
