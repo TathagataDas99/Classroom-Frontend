c5a908a569bcef0aaf5b4abb370c1c44c3763f17
Tathagata Das <dastathagata9919@gmail.com>
2022-05-13 21:34:53 +0530

    Teacher dashboard done

--- src/views/Teacher/TeacherDashboard.vue
+++ src/views/Teacher/TeacherDashboard.vue
@@ -1,11 +1,96 @@
 <template>
   <div>
-    <h1>Hello teacher</h1>
+    <div v-if="loader">
+      <LoaderCard />
+    </div>
+    <section v-else>
+      <!-- {{ userProfile.classroom_list }} -->
+      <div
+        class="card"
+        v-for="classroom in userProfile.classroom_list"
+        :key="classroom.slug"
+      >
+        <div class="card-title text-2xl text-zinc-700">
+          {{ classroom.stream }}
+        </div>
+        <div class="card-title text-2xl text-zinc-700">
+          {{ classroom.title }}
+        </div>
+        <div class="card-title text-2xl text-zinc-700">
+          {{ classroom.start_year }}
+        </div>
+        <div class="card-title text-2xl text-zinc-700">
+          {{ classroom.end_year }}
+        </div>
+        <div class="card-title text-2xl text-zinc-700">
+          {{ classroom.section }}
+        </div>
+        <div class="card-title text-2xl text-zinc-700">
+          {{ classroom.level }}
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+// import LoaderView from "../../components/LoaderView.vue";
+import { mapGetters } from "vuex";
+import LoaderCard from "../../components/LoaderCard.vue";
+export default {
+  name: "StudentDashboard",
+  data() {
+    return {
+      msg: "",
+      loader: "",
+    };
+  },
+  components: {
+    LoaderCard,
+  },
+  computed: {
+    ...mapGetters(["userType", "userProfile"]),
+  },
+  async created() {
+    this.loader = true;
+    try {
+      const response1 = await axios.get("/auth/users/me/", {
+        headers: {
+          Authorization: "JWT " + sessionStorage.getItem("token"),
+        },
+      });
+      const userId = response1.data.id;
+      const userTypeResponse = await axios.get(
+        `/classroom-app/user-type/${userId}`
+      );
+      const userTypeObj = {
+        userType: userTypeResponse.data.user_type,
+        userTypeId: Object.values(userTypeResponse.data)[1],
+      };
+      this.$store.dispatch("userType", userTypeObj);
+
+      const userProfileResponse = await axios.get(
+        `/classroom-app/teacher/${userTypeObj.userTypeId}/`
+      );
+      this.$store.dispatch("userProfile", userProfileResponse.data);
+      console.log(this.userProfile);
+    } catch (e) {
+      console.log(e);
+    }
+    this.loader = false;
+  },
+  methods: {
+    handelOpen(no) {
+      this.$router.push({
+        name: "SubjectCards",
+        params: {
+          no: no,
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
