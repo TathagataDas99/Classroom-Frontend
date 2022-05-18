d71a5ab64882c6b85698a49bc1a88ea731f67db4
PriyeshGhosh <Priyeshghosh1997@gmail.com>
2022-05-18 13:03:38 +0530

    font and colour theme created

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,38 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js}", "./src/*.{vue, js}"],
   theme: {
     extend: {
+      fontFamily:{
+        heading: "'Lora', serif",
+        body: "'Nunito', sans-serif"
+      },
       colors: {
+        bgdark: {
+          base : "1E293B",
+          shade: "64748B",
+        },
+        bglight: {
+          base : "F8FAFC",
+          shade: "E2E8F0",
+        },
+        
+
         primary: {
-          dark: "#00BFA6",
-          light: "#3af0d1",
+          dark: "#0BDA9D",
+          light: "#05F3A9",
         },
         danger: {
-          dark: "#F50057",
-          light: "#f2384e",
+          dark: "#F5003D",
+          light: "#EC1D64",
         },
+        info:{
+          dark:"00B0FF",
+          light:"4CCAF1"
+        },
+        warning:{
+          dark:"F9A826",
+          light:"F3B155"
+
+        }
       },
     },
   },
