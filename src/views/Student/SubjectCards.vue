4aef6a0f4bfa8c0a7396b8e69e456c452f060f59
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-24 18:37:58 +0530

    subject page done

--- src/views/Student/SubjectCards.vue
+++ src/views/Student/SubjectCards.vue
@@ -1,33 +1,79 @@
 <template>
-  <div
-    class="mt-5 grid grid-flow-row grid-cols-1 items-center justify-evenly gap-3 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-3"
-  >
-    <template v-if="loader">
-      <template v-for="i in 8" :key="i">
-        <LoaderCard class="col-span-1 row-span-1 place-self-center" />
-      </template>
-    </template>
-    <section
-      class="card col-span-1 row-span-1 place-self-center"
-      v-for="subject in subjects"
-      :key="subject.slug"
+  <main class="flex flex-col items-center justify-evenly bg-bglight-base">
+    <div
+      class="mt-6 flex w-3/5 flex-col items-center justify-between rounded-2xl border-l-8 border-b-4 border-danger-dark px-10 py-3 font-heading text-2xl shadow-lg md:flex-row"
     >
-      <h1 class="card-title text-2xl text-zinc-700">
-        {{ subject.title }}
-      </h1>
-      <h1 class="card-title text-sm text-zinc-700">
-        {{ subject.subject_code }}
-      </h1>
-      <span class="text-2xl font-semibold text-zinc-700"
-        >{{ subject.created_by.user.first_name }}
-        {{ subject.created_by.user.last_name }}</span
+      <p>Semester - {{ no }}</p>
+      <router-link
+        class="link-danger slow-effect text-center font-body font-medium underline shadow-md"
+        :to="{ name: 'StudentDashboard' }"
+        >Go To Semester Page</router-link
       >
-      <button class="btn" @click="handelOpen(this.no, subject.slug)">
-        Open
-      </button>
-    </section>
-    <!-- {{ subjects }} -->
-  </div>
+    </div>
+    <div
+      class="mt-5 grid min-h-screen w-screen grid-flow-row grid-cols-1 items-center justify-evenly gap-3 bg-bglight-base md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-3"
+    >
+      <template v-if="loader">
+        <template v-for="i in 8" :key="i">
+          <LoaderCard class="col-span-1 row-span-1 place-self-center" />
+        </template>
+      </template>
+      <section
+        class="SubjectCard group place-self-center"
+        v-for="subject in subjects"
+        :key="subject.slug"
+      >
+        <div class="SubjectCard-section">
+          <h1
+            class="subject-card-title text-primary-light group-hover:text-warning-dark"
+          >
+            Subject :
+          </h1>
+          <h2 class="subject-card-title">
+            {{ subject.title }}
+          </h2>
+        </div>
+        <div class="SubjectCard-section">
+          <h1
+            class="subject-card-title text-primary-light group-hover:text-warning-dark"
+          >
+            Code :
+          </h1>
+          <h2 class="subject-card-title">
+            {{ subject.subject_code }}
+          </h2>
+        </div>
+        <div class="SubjectCard-section">
+          <h1
+            class="subject-card-title text-primary-light group-hover:text-warning-dark"
+          >
+            Credit Points :
+          </h1>
+          <h2 class="subject-card-title">
+            {{ subject.credit_points }}
+          </h2>
+        </div>
+        <div class="SubjectCard-section">
+          <h1
+            class="subject-card-title text-primary-light group-hover:text-warning-dark"
+          >
+            Teacher :
+          </h1>
+          <h2 class="subject-card-title">
+            {{ subject.created_by.user.first_name }}
+            {{ subject.created_by.user.last_name }}
+          </h2>
+        </div>
+        <button
+          class="bttn place-self-end group-hover:bg-warning-dark"
+          @click="handelOpen(this.no, subject.slug)"
+        >
+          View
+        </button>
+      </section>
+      <!-- {{ subjects }} -->
+    </div>
+  </main>
 </template>
 
 <script>
