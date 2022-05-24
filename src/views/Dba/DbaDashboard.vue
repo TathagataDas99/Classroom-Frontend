e6e44f6f1249f1c947a6a97db6702fe1518fc2ac
Tathagata Das <dastathagata9919@gmail.com>
2022-05-24 22:27:53 +0530

    dba started

--- src/views/Dba/DbaDashboard.vue
+++ src/views/Dba/DbaDashboard.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
-    <h1>Hello Dba.</h1>
+    <h1>{{ userProfile }}</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import { mapGetters } from "vuex";
+
+export default {
+  async created() {
+    this.loader = true;
+    try {
+      const response1 = await axios.get("/auth/users/me/", {
+        headers: {
+          Authorization: "JWT " + sessionStorage.getItem("token"),
+        },
+      });
+      const userId = response1.data.id;
+      const userTypeResponse = await axios.get(
+        `/classroom-app/user-type/${userId}`
+      );
+      const userTypeObj = {
+        userType: userTypeResponse.data.user_type,
+        userTypeId: Object.values(userTypeResponse.data)[1],
+      };
+      this.$store.dispatch("userType", userTypeObj);
+
+      const userProfileResponse = await axios.get(
+        `/classroom-app/dba/${userTypeObj.userTypeId}/`
+      );
+      this.$store.dispatch("userProfile", userProfileResponse.data);
+    } catch (e) {
+      console.log(e);
+    }
+    this.loader = false;
+  },
+  computed: {
+    ...mapGetters(["userType", "userProfile"]),
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
