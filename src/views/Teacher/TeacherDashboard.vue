055443012097b173f420b7ae8c39387ba5d7b4a6
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-25 20:30:06 +0530

    teacher cards partial done

--- src/views/Teacher/TeacherDashboard.vue
+++ src/views/Teacher/TeacherDashboard.vue
@@ -1,36 +1,100 @@
 <template>
-  <div>
+  <div
+    class="grid min-h-screen grid-flow-row md:grid-flow-col md:grid-cols-5 md:grid-rows-1 lg:grid-cols-8"
+  >
     <div v-if="loader">
       <LoaderCard />
     </div>
-    <section v-else>
-      {{ userProfile }}
-      <div
-        class="card"
+    <main
+      v-else
+      class="grid grid-flow-row grid-cols-1 place-content-center place-items-stretch justify-items-center gap-3 md:col-span-3 md:grid-cols-2 md:grid-rows-2 lg:col-span-5"
+    >
+      <!-- {{ userProfile }} -->
+      <template
         v-for="classroom in userProfile.classroom_list"
         :key="classroom.slug"
       >
-        <div class="card-title text-2xl text-zinc-700">
-          {{ classroom.stream }}
+        <section class="col-span-1 row-span-1">
+          <div class="ClassroomCard">
+            <div class="text-2xl text-zinc-700">
+              {{ classroom.stream }}
+            </div>
+            <div class="text-2xl text-zinc-700">
+              {{ classroom.title }}
+            </div>
+            <div class="text-2xl text-zinc-700">
+              {{ classroom.start_year }}
+            </div>
+            <div class="text-2xl text-zinc-700">
+              {{ classroom.end_year }}
+            </div>
+            <div class="text-2xl text-zinc-700">
+              {{ classroom.section }}
+            </div>
+            <div class="card-title text-2xl text-zinc-700">
+              {{ classroom.level }}
+            </div>
+            <div class="button-section">
+              <button
+                class="bttn place-self-end"
+                @click="handelOpen(classroom.slug)"
+              >
+                Open
+              </button>
+            </div>
+          </div>
+        </section>
+      </template>
+    </main>
+    <aside
+      class="row-start-2 my-2 mx-2 grid grid-cols-1 grid-rows-3 gap-2 md:col-span-2 md:row-start-1 lg:col-span-3 lg:row-start-auto"
+    >
+      <section
+        tabindex="0"
+        :class="{ 'collapse-open': !contactEdit }"
+        class="slow-effect collapse focus:shadow-md"
+      >
+        <div class="collapse-title-addition collapse-title">
+          <span class="font-semibold text-danger-dark">Welcome</span>
+          {{ userProfile.user.first_name }} {{ userProfile.user.last_name }}
         </div>
-        <div class="card-title text-2xl text-zinc-700">
-          {{ classroom.title }}
+        <div class="collapse-content-layout collapse-content">
+          <span class="collapse-content-label">Email : </span>
+          <p class="col-span-3">
+            {{ userProfile.user.email }}
+          </p>
+          <span class="collapse-content-label">Contact </span>
+          <p class="col-span-2">
+            <input
+              minlength="10"
+              maxlength="10"
+              v-if="userProfile.user.contact_no || !contactEdit"
+              class="w-full bg-bglight-shade px-4 outline-none focus:invalid:text-danger-dark"
+              :class="{
+                'mr-4 border-b-2 border-primary-light focus:invalid:border-danger-dark':
+                  !contactEdit,
+              }"
+              type="tel"
+              :disabled="contactEdit"
+              v-model="userProfile.user.contact_no"
+            />
+            <template v-else>
+              <span class="col-span-3">Not Available</span>
+            </template>
+          </p>
+          <PencilIcon
+            class="collapse-content-icon"
+            @click="contactEdit = !contactEdit"
+            v-if="contactEdit"
+          />
+          <CheckCircleIcon
+            class="collapse-content-icon text-primary-light"
+            v-if="!contactEdit"
+            @click="editContact"
+          />
         </div>
-        <div class="card-title text-2xl text-zinc-700">
-          {{ classroom.start_year }}
-        </div>
-        <div class="card-title text-2xl text-zinc-700">
-          {{ classroom.end_year }}
-        </div>
-        <div class="card-title text-2xl text-zinc-700">
-          {{ classroom.section }}
-        </div>
-        <div class="card-title text-2xl text-zinc-700">
-          {{ classroom.level }}
-        </div>
-        <button class="btn" @click="handelOpen(classroom.slug)">Open</button>
-      </div>
-    </section>
+      </section>
+    </aside>
   </div>
 </template>
 
@@ -38,16 +102,20 @@
 import axios from "axios";
 import { mapGetters } from "vuex";
 import LoaderCard from "../../components/LoaderCard.vue";
+import { PencilIcon, CheckCircleIcon } from "@heroicons/vue/solid";
 export default {
   name: "TeacherDashboard",
   data() {
     return {
+      contactEdit: true,
       msg: "",
       loader: "",
     };
   },
   components: {
     LoaderCard,
+    PencilIcon,
+    CheckCircleIcon,
   },
   computed: {
     ...mapGetters(["userType", "userProfile"]),
@@ -81,6 +149,15 @@ export default {
     this.loader = false;
   },
   methods: {
+    editContact() {
+      this.contactEdit = !this.contactEdit;
+      // TODO: implement this function
+      // try{
+      //   const response = await axios.patch('')
+      // } catch(error){
+      //   console.log(error)
+      // }
+    },
     handelOpen(slug) {
       this.$router.push({
         name: "ClassroomView",
