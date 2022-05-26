d1b06caf339d2b160c84a9eab3ba539bdda65882
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-26 16:26:45 +0530

    design done

--- src/views/Teacher/Subjects/particularSubjectView.vue
+++ src/views/Teacher/Subjects/particularSubjectView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="h-screen">
+  <div
+    class="flex h-screen flex-col items-center justify-evenly bg-bglight-base"
+  >
     <div v-if="loader">
       <LoaderView />
     </div>
-
+    <!-- {{ semCards }} -->
     <div
-      class="flex flex-col items-stretch justify-between px-3 pt-3 md:flex-row"
+      class="grid w-full flex-shrink grid-rows-2 place-content-center md:grid-cols-5 md:grid-rows-1"
     >
-      <nav class="tabs w-full md:w-1/2">
+      <nav class="tabs col-span-1 place-self-center md:col-span-2">
         <router-link
           :to="{
             name: 'announcementTeacher',
@@ -17,16 +19,16 @@
               subject_slug,
             },
           }"
-          class="tab tab-lifted md:tab-md"
+          class="tab tab-lifted tab-sm lg:tab-lg"
           :class="{
             'tab-active': isActive === 1,
-            'font-semibold text-primary-light': isActive === 1,
+            'tab-active-dynamic': isActive === 1,
           }"
           @click="activeTab(1)"
           >Announcement</router-link
         >
         <router-link
-          class="tab tab-lifted md:tab-md"
+          class="tab tab-lifted tab-sm lg:tab-lg"
           :to="{
             name: 'notesTeacher',
             props: {
@@ -37,13 +39,13 @@
           }"
           :class="{
             'tab-active': isActive === 2,
-            'font-semibold text-primary-light': isActive === 2,
+            'tab-active-dynamic': isActive === 2,
           }"
           @click="activeTab(2)"
           >Notes</router-link
         >
         <router-link
-          class="tab tab-lifted md:tab-md"
+          class="tab tab-lifted tab-sm lg:tab-lg"
           :to="{
             name: 'assingmentTeacher',
             props: {
@@ -54,15 +56,22 @@
           }"
           :class="{
             'tab-active': isActive === 3,
-            'font-semibold text-primary-light': isActive === 3,
+            'tab-active-dynamic': isActive === 3,
           }"
           @click="activeTab(3)"
           >Assignments</router-link
         >
       </nav>
-      <div class="mr-7">
+      <div
+        class="row-start-2 hidden md:col-span-2 md:col-start-3 md:row-start-1 md:block"
+      >
+        <div class="page-header-2">
+          <p>Subject - {{ subject_slug }}</p>
+        </div>
+      </div>
+      <div class="col-span-1 row-start-1 place-self-center md:col-start-5">
         <button
-          class="btn"
+          class="link-danger columns-1"
           @click="
             $router.replace({
               name: 'TeacherSubjectCards',
@@ -71,11 +80,12 @@
             })
           "
         >
-          Go Back
+          <ReplyIcon class="inline-block w-6 text-danger-dark" />
+          Subject Page
         </button>
       </div>
     </div>
-    <router-view />
+    <router-view class="flex-1" />
   </div>
 </template>
 
@@ -83,6 +93,7 @@
 import axios from "axios";
 import LoaderView from "../../../components/LoaderView.vue";
 import { mapGetters } from "vuex";
+import { ReplyIcon } from "@heroicons/vue/solid";
 export default {
   props: ["classroom_slug", "no", "subject_slug"],
   data() {
@@ -95,6 +106,7 @@ export default {
   },
   components: {
     LoaderView,
+    ReplyIcon,
   },
   computed: {
     ...mapGetters(["userType", "userProfile", "semCards"]),
