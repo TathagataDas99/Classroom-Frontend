83b8ddb1405149ef8e437bee7f442331203228c6
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-05-29 01:15:41 +0530

    linted

--- src/views/Teacher/Subjects/AssignmentSubmission.vue
+++ src/views/Teacher/Subjects/AssignmentSubmission.vue
@@ -1,15 +1,11 @@
 <template>
-    <div>
-        <h1>Hello Submission.</h1>
-    </div>
+  <div>
+    <h1>Hello Submission.</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
