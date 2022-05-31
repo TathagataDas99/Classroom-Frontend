560ce1291eb7be6cc77b148eb9381f0c700fd868
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-06-01 01:28:01 +0530

    loader card redesigned

--- src/components/LoaderCard.vue
+++ src/components/LoaderCard.vue
@@ -1,11 +1,22 @@
 <template>
-  <section
-    class="relative grid h-3/5 w-3/5 grid-flow-row grid-cols-9 grid-rows-6 place-items-center place-self-center overflow-clip rounded-lg bg-slate-400 backdrop-blur-md backdrop-filter"
-  >
-    <img
-      class="col-span-7 col-start-2 row-span-4 row-start-2 animate-pulse rounded-lg bg-slate-500"
-      src="../assets/components/card_img_placeholder_1.svg"
-    />
+  <section class="h-4/6 w-3/6 overflow-clip rounded-xl bg-slate-200 shadow-md">
+    <article
+      class="h-1/2 w-full animate-pulse bg-slate-400 delay-200"
+    ></article>
+    <div
+      class="my-2 mx-1 h-3 w-3/5 animate-pulse rounded-3xl bg-zinc-200 delay-300"
+    ></div>
+    <div
+      class="my-2 mx-1 h-3 w-4/6 animate-pulse rounded-3xl bg-bgdark-shade delay-75"
+    ></div>
+    <div
+      class="my-2 mx-1 h-3 w-4/5 animate-pulse rounded-3xl bg-gray-300 delay-100"
+    ></div>
+    <div class="flex flex-row items-center justify-evenly">
+      <button
+        class="h-10 w-2/4 animate-pulse rounded-xl bg-gray-500/60 align-middle"
+      ></button>
+    </div>
   </section>
 </template>
 
