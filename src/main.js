435d091667854339b0de339cb65c8642efe7329d
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-06-03 17:15:33 +0530

    DatePicker Component added

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import "@/css/index.css";
 import "@/css/style.css";
 import "./axios";
 import store from "./store";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
 const app = createApp(App);
+
+// eslint-disable-next-line vue/multi-word-component-names
+app.component("Datepicker", Datepicker);
 
 app.use(router);
 
