1003e0f9738ca62bfef5c1a227f2aaea2f0f8493
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-06-05 17:58:03 +0530

    glitch solved & is late made dynamic

--- src/views/Teacher/Subjects/AssignmentSubmission.vue
+++ src/views/Teacher/Subjects/AssignmentSubmission.vue
@@ -23,20 +23,26 @@
           <th>Score</th>
           <th>Remarks</th>
         </tr>
+        <tr v-if="submissions.length === 0">
+          <td colspan="5" class="text-lg font-bold text-danger-dark">
+            No Submissions Yet
+          </td>
+        </tr>
         <tr
+          v-else
           v-for="(submission, index) in submissions"
           :key="submission.id"
-          class="my-2 mx-1 rounded-lg border-x-2 border-gray-200 px-3 py-2 odd:bg-sky-100 even:bg-green-100"
+          class="my-2 mx-1 rounded-lg border-x-2 border-gray-200 px-3 py-2 text-center odd:bg-sky-100 even:bg-green-100"
         >
           <td class="text-center">
             <ClockIcon
               class="w-7 text-bgdark-base"
               :class="{
-                'text-primary-dark': this.flag,
-                'text-danger-dark': !this.flag,
+                'text-primary-dark': this.flagArr[index],
+                'text-danger-dark': !this.flagArr[index],
               }"
-              @click="isLate(submission)"
             />
+            <!-- @click="isLate(submission, index)" -->
             <!-- <ExclamationCircleIcon
               class="w-6 bg-danger-dark text-bglight-shade"
             
@@ -131,6 +137,7 @@ export default {
   data() {
     return {
       // subjectEdit:true,
+      flagArr: [],
       flag: false,
       loader: false,
       subjectEditArr: [],
@@ -142,33 +149,39 @@ export default {
   computed: {
     ...mapGetters(["userType", "userProfile", "semCards"]),
   },
-  async beforeUpdate() {
-    // this.loader = true;
-    //  this.flag = false;
-    const submissionsResp = await axios.get(
-      `/classroom-app/teacher/${this.userProfile.teacher_id}/subject/${this.subject_slug}/assignment/${this.id}/submission/`
-    );
-    this.submissions = submissionsResp.data;
-    for (let i of this.submissions) {
-      this.subjectEditArr.push(true);
-      this.formValueList.push({
-        has_scored: true,
-        score: i.score,
-        remarks: i.remarks,
-      });
-    }
-    // this.loader = false; //NOT WORKING
-  },
+  // async updated() {
+  //   // this.loader = true;
+  //   //  this.flag = false;
+  //   console.log("in update");
+  //   const submissionsResp = await axios.get(
+  //     `/classroom-app/teacher/${this.userProfile.teacher_id}/subject/${this.subject_slug}/assignment/${this.id}/submission/`
+  //   );
+  //   this.submissions = submissionsResp.data;
+  //   for (let i of this.submissions) {
+  //     this.subjectEditArr.push(true);
+  //     this.flagArr.push(false);
+  //     this.formValueList.push({
+  //       has_scored: true,
+  //       score: i.score,
+  //       remarks: i.remarks,
+  //     });
+  //   }
+  //   // this.loader = false; //NOT WORKING
+  // },
 
-  async created() {
+  async mounted() {
     // this.flag = false;
     this.loader = true;
+    console.log("in be4 Mount");
     const submissionsResp = await axios.get(
       `/classroom-app/teacher/${this.userProfile.teacher_id}/subject/${this.subject_slug}/assignment/${this.id}/submission/`
     );
     this.submissions = submissionsResp.data;
+    let c = 0;
     for (let i of this.submissions) {
       this.subjectEditArr.push(true);
+      let k = this.isLate(i, c++);
+      this.flagArr.push(k);
       this.formValueList.push({
         has_scored: true,
         score: i.score,
@@ -177,39 +190,61 @@ export default {
     }
     this.loader = false;
   },
-
+  watch: {
+    id: async function (newvalue) {
+      this.loader = true;
+      console.log("in be4 Mount");
+      const submissionsResp = await axios.get(
+        `/classroom-app/teacher/${this.userProfile.teacher_id}/subject/${this.subject_slug}/assignment/${newvalue}/submission/`
+      );
+      this.submissions = submissionsResp.data;
+      let c = 0;
+      for (let i of this.submissions) {
+        let k = this.isLate(i, c++);
+        this.flagArr.push(k);
+        this.subjectEditArr.push(true);
+        this.flagArr.push(false);
+        this.formValueList.push({
+          has_scored: true,
+          score: i.score,
+          remarks: i.remarks,
+        });
+      }
+      this.loader = false;
+    },
+  },
   methods: {
-    async isLate(submission) {
-      this.flag = false;
+    async isLate(submission, index) {
+      this.flagArr[index] = false;
       try {
         const assignmentResp = await axios.get(
           `/classroom-app/teacher/${this.userProfile.teacher_id}/subject/${this.subject_slug}/assignment/${this.id}/`
         );
         const assignment = assignmentResp.data;
-        console.log("Assignment time");
-        console.log(assignment.due_time);
-        console.log("Submission time");
-        console.log(submission.submission_time.split(".")[0]);
+        // console.log("Assignment time");
+        // console.log(assignment.due_time);
+        // console.log("Submission time");
+        // console.log(submission.submission_time.split(".")[0]);
         // console.log("Submission_date");
         // console.log(Date.parse(submission.submission_date));
 
-        console.log(
-          new Date(assignment.due_date) >=
-            new Date(submission.submission_date) &&
-            assignment.due_time >= submission.submission_time.split(".")[0]
-        );
+        // console.log(
+        //   new Date(assignment.due_date) >=
+        //     new Date(submission.submission_date) &&
+        //     assignment.due_time >= submission.submission_time.split(".")[0]
+        // );
         if (
           new Date(assignment.due_date) >=
             new Date(submission.submission_date) &&
           assignment.due_time >= submission.submission_time.split(".")[0]
         ) {
-          this.flag = true;
+          this.flagArr[index] = true;
         }
       } catch (e) {
         console.log(e);
       }
-      console.log("flag");
-      console.log(this.flag);
+      // console.log("flag");
+      // console.log(this.flag);
       // return flag;
     },
     async editPatch(submission, index) {
